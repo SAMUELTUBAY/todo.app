427a6d1eefddae4ef1a19e17b36973244b6d072d
SAMUELTUBAY <samuel.tubay@tecnologicoliceocristiano.edu.ec>
2025-05-07 21:33:14 -0500

    mi primer en todo-app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 import routertodos from "./routes/todos.routes.js";
 
 
 const app =express();
+
+
 app.use(express.json());
 
+app.use(morgan.json("dev"));
+
+
 app.use(routertodos);
+
 
 
 app.listen(3000, () =>console.log("Listening on port 3000"));
